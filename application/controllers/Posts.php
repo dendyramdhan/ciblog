da40e9b2287e6c3578c7f35e1935f681639ce386
Dendy Ramdhan <ramdhandendy@gmail.com>
2019-02-13 19:55:23 +0700

    Create and Delete post

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -33,8 +33,22 @@ class Posts extends CI_Controller {
     {
         $data['title'] = 'Create Post';
 
-        $this->load->view('templates/header');
-        $this->load->view('posts/create', $data);
-        $this->load->view('templates/footer');
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('body', 'Body', 'required');
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('templates/header');
+            $this->load->view('posts/create', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $this->post_model->create_post();
+            redirect('posts');
+        }
+    }
+
+    public function delete($id)
+    {
+        $this->post_model->delete_post($id);
+        redirect('posts');
     }
 }
--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -1,1 +1,16 @@
-<h2><?= $title; ?></h2>
+<?= validation_errors(); ?>
+
+<?= form_open('posts/create'); ?>
+  <fieldset>
+    <legend><?= $title; ?></legend>  
+    <div class="form-group">
+      <label class="col-form-label" for="title">Title</label>
+      <input name="title" type="text" class="form-control" placeholder="Add Title" id="title">
+    </div>
+    <div class="form-group">
+      <label class="col-form-label" for="body">Body</label>
+      <textarea name="body" class="form-control" id="body" rows="3" placeholder="Add Body"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </fieldset>
+</form>
